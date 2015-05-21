c82498e73ac8eef840181ba57232ef8c8b1c3bca
Timothy McFadden <timothy.mcfadden@avagotech.com>
2015-05-21 12:26:27 -0600

    Changing the name of the object

--- settings.go
+++ settings.go
@@ -1,50 +1,50 @@
-package settings
+// This package is used as an interface to various application settings stored
+// in a JSON file or string.
+package go_settings
 
 import (
 	"encoding/json"
 	"fmt"
-	// "strings"
 	"io/ioutil"
 	"os"
 	"regexp"
 )
 
-type Config struct {
+type Settings struct {
 	_map map[string]interface{}
 }
 
-func (c Config) Get(item string) interface{} {
+func (c Settings) Get(item string) interface{} {
 	return c._map[item]
 }
 
+// removeComments parses a byte array and replaces all text following
+// double-forward-slashes with a space character.
 func removeComments(bytes []byte) []byte {
 	bytes = regexp.MustCompile("(//.*)").ReplaceAll(bytes, []byte{32})
 	return bytes
 }
 
-func NewConfig(bytes []byte) Config {
+// Create a new Settings object from a byte array
+func NewSettings(bytes []byte) Settings {
 
 	// Create a generic interface
 	var f interface{}
 
+	new_bytes := removeComments(bytes)
+
 	// Unmarshal the bytes of the JSON data into the generic interface
-	json.Unmarshal(bytes, &f)
+	json.Unmarshal(new_bytes, &f)
 
 	// Create the object with a string map() of the unmarshalled data
-	return Config{f.(map[string]interface{})}
+	return Settings{f.(map[string]interface{})}
 }
 
-func NewConfigFromFile(filename string) Config {
+// Create a new Settings object from a JSON file
+func NewSettingsFromFile(filename string) Settings {
 	file, _ := os.Open(filename)
 	b, _ := ioutil.ReadAll(file)
 
-	newb := removeComments(b)
-	return NewConfig(newb)
-}
-
-func main_settings() {
-	c := NewConfigFromFile("config.json")
-	fmt.Println(c.Get("logger.enabled"))
-	fmt.Println(c.Get("logger.debug"))
-	fmt.Println(c.Get("users"))
+	new_bytes := removeComments(b)
+	return NewSettings(new_bytes)
 }
